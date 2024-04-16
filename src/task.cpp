65546abc4ee52b7090f5b1e76c68e68be25a1111
Reza-namvaran <reza.namvaran823@gmail.com>
2024-04-16 14:29:37 +0330

    Fixed task.cpp bugs

--- src/task.cpp
+++ src/task.cpp
@@ -1,22 +1,22 @@
 #include "task.hpp"
 
-Tasl::Task(const std::string& p_name, const char& p_level, const int& p_time) {
+Task::Task(const std::string& p_name, const char& p_level, const int& p_time) {
     this->setTaskName(p_name);
     this->setDifficulty(p_level);
     this->setTime(p_time);
 }
 
 void Task::setTaskName(const std::string& p_name) {
-    /// TODO Validation
+    /// TODO: Validation
     this->task_name = p_name;
 }
 
-void setDifficulty(const char& p_level) {
-    /// TODO Validation
+void Task::setDifficulty(const char& p_level) {
+    /// TODO: Validation
     this->difficulty_level = p_level; 
 }
 
-void setTime(const int& p_time) {
-    /// TODO Validation
+void Task::setTime(const int& p_time) {
+    /// TODO: Validation
     this->time_to_done = p_time;
 }
